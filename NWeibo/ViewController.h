64db9b239a2cf7a9f1b41d3a99a8729f7005667a
furui <furui@rijin.com>
2015-10-11 10:16:35 +0800

    Frist commit

---
+++ NWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NWeibo
+//
+//  Created by furui on 15/10/11.
+//  Copyright © 2015年 furui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NWeibo
+//
+//  Created by furui on 15/10/11.
+//  Copyright © 2015年 furui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
